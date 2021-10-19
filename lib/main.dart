f6d162d5e346a40029484080390a79a625eb8f94
Erick Njoroge <erick.njoroge@betika.com>
2021-10-20 01:44:11 +0300

    add theme for dark mode and light mode

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fooderlich',
+      // TODO: Create theme
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Fooderlich'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // TODO: Style the title
+        title: Text(widget.title),
+      ),
+      // TODO: Style the body text
+      body: const Center(
+        child: Text(
+          'Let\'s get cooking 👩‍🍳',
+        ),
+      ),
+    );
+  }
+}
