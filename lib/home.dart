460b589a404fe5144222081b3e4c52bb0d2033ac
Erick Njoroge <erick.njoroge@betika.com>
2021-10-20 03:02:48 +0300

    add navigable pages to bottom navigation

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    //TODO: Replace with Card1
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.blue)
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +35,12 @@ class _HomeState extends State<Home> {
         ),
       ),
       // TODO: Show selected tab
-      body: Center(
-        child: Text(
-          'Let\'s get cooking 👩‍🍳',
-          style: Theme.of(context).textTheme.headline1,
-        ),
-      ),
+      body: pages[_selectedIndex],
       // TODO: Add bottom navigation bar
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         items: <BottomNavigationBarItem>[
           const BottomNavigationBarItem(
               icon: Icon(Icons.card_giftcard), label: 'Card'),
