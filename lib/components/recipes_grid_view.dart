3b409a602cee4040832ac8c03804f73ab79a12d4
Erick Njoroge <ericnjoroge3@gmail.com>
2021-11-06 10:05:15 +0300

    add recipes grid view list

---
+++ lib/components/recipes_grid_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../components/components.dart';
+import '../models/models.dart';
+
+class RecipesGridView extends StatelessWidget {
+  final List<SimpleRecipe> recipes;
+  const RecipesGridView({
+    Key? key,
+    required this.recipes,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
