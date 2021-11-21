37e02687e1603c300aa0531a17f493d0fe77b1f4
Erick Njoroge <ericnjoroge3@gmail.com>
2021-11-22 00:39:53 +0300

    add grocery item tile

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 import '../models/models.dart';
+import '../components/grocery_tile.dart';
 
 class GroceryItemsScreen extends StatefulWidget {
   final Function(GroceryItem) onCreate;
@@ -103,6 +104,20 @@ class _GroceryItemsScreenState extends State<GroceryItemsScreen> {
             ),
             buildQuantityField(),
             //TODO: Add Grocery Tile
+            GroceryTile(
+                item: GroceryItem(
+                    id: 'previewMode',
+                    name: _name,
+                    importance: _importance,
+                    color: _currentColor,
+                    quantity: _currentSliderValue,
+                    date: DateTime(
+                      _dueDate.year,
+                      _dueDate.month,
+                      _dueDate.day,
+                      _timeOfDay.hour,
+                      _timeOfDay.minute,
+                    )))
           ],
         ),
       ),
